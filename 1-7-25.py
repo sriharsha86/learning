737fc453b3c2b9ef5073a29e6ab1a0fdd8cc09a5
Harsha <hk.harshapshv@gmail.com>
2025-07-01 18:22:26 +0530

    3rd question

--- 1-7-25.py
+++ 1-7-25.py
@@ -15,3 +15,14 @@ def add(a, b):
     return a+b
 result = add(5, 3)
 print(result) 
+
+'''Write a function to check if a numebr is prime'''
+def is_prime(n):
+    if n<=1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(12))
