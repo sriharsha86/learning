369b220e007ace32e203c631783f4958fe7b350d
Harsha <hk.harshapshv@gmail.com>
2025-07-01 17:31:37 +0530

    first question

---
+++ 1-7-25.py
@@ -0,0 +1,8 @@
+''' What is a function in Python?
+    A function is a reusable block of code that performs a specific task'''
+
+#example code :
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Alice")
