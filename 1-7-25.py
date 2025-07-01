65dbe566ab8180bdcb96ee26db03c79db7f6b4c5
Harsha <hk.harshapshv@gmail.com>
2025-07-01 17:37:08 +0530

    second question

--- 1-7-25.py
+++ 1-7-25.py
@@ -6,3 +6,12 @@ def greet(name):
     print(f"Hello, {name}!")
 
 greet("Alice")
+
+''' 2. What is the main difference between return and print in a function?
+    print() displays the result to the console
+    return sends the result back to the caller for further use'''
+
+def add(a, b):
+    return a+b
+result = add(5, 3)
+print(result) 
